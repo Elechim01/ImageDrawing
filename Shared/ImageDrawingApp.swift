49fddeb43cc95a098ccfe930e686c254a47b3bbc
Elechim01 <mikymanny015@gmail.com>
2021-04-23 22:28:17 +0200

    Initial Commit

---
+++ Shared/ImageDrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageDrawingApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 23/04/21.
+//
+
+import SwiftUI
+
+@main
+struct ImageDrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
